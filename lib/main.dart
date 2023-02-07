fd775fc95b921b9d8a70b1bdf7af6c412708ec5e
dinhtai98 <vodinhtai0@gmail.com>
2023-02-07 13:52:08 +0700

    #20 splash done

--- lib/main.dart
+++ lib/main.dart
@@ -27,8 +27,6 @@ Future<void> mainDelegate() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
